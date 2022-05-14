e260d18b0351f23d23de89daa7230ddb8b3ba7e9
korzepadawid <korzepadawid@yahoo.com>
2022-05-14 17:06:09 +0200

    feat: navigation between login and register activities

--- app/src/main/java/io/github/korzepadawid/chatapplication_lsm/LoginActivity.kt
+++ app/src/main/java/io/github/korzepadawid/chatapplication_lsm/LoginActivity.kt
@@ -1,11 +1,33 @@
 package io.github.korzepadawid.chatapplication_lsm
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var emailEditText: EditText
+    private lateinit var passwordEditText: EditText
+    private lateinit var loginButton: Button
+    private lateinit var registerButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        initializeUi()
+    }
+
+    private fun initializeUi() {
+        emailEditText = findViewById(R.id.edit_text_email_login)
+        passwordEditText = findViewById(R.id.edit_text_password_login)
+        loginButton = findViewById(R.id.button_log_in_login)
+        registerButton = findViewById(R.id.button_register_login)
+
+        registerButton.setOnClickListener {
+            val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/io/github/korzepadawid/chatapplication_lsm/RegisterActivity.kt
+++ app/src/main/java/io/github/korzepadawid/chatapplication_lsm/RegisterActivity.kt
@@ -1,11 +1,33 @@
 package io.github.korzepadawid.chatapplication_lsm
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var emailEditText: EditText
+    private lateinit var passwordEditText: EditText
+    private lateinit var loginButton: Button
+    private lateinit var registerButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        initializeUi()
+    }
+
+    private fun initializeUi() {
+        emailEditText = findViewById(R.id.edit_text_email_register)
+        passwordEditText = findViewById(R.id.edit_text_password_register)
+        loginButton = findViewById(R.id.button_log_in_register)
+        registerButton = findViewById(R.id.button_register_register)
+
+        loginButton.setOnClickListener {
+            val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
