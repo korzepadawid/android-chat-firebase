08ac895ab8803961c3bb500a345985ee0fe1f82f
korzepadawid <korzepadawid@yahoo.com>
2022-05-14 16:08:02 +0200

    feat: login and register activities

---
+++ app/src/main/java/io/github/korzepadawid/chatapplication_lsm/LoginActivity.kt
@@ -0,0 +1,11 @@
+package io.github.korzepadawid.chatapplication_lsm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
