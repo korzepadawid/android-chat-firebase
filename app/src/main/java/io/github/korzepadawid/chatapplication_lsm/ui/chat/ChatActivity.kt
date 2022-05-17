f29e11e4d0963306405ab7fd38ef91e78952ffd9
korzepadawid <korzepadawid@yahoo.com>
2022-05-17 14:32:56 +0200

    feat: disabled send message button when message box is blank

--- app/src/main/java/io/github/korzepadawid/chatapplication_lsm/ui/chat/ChatActivity.kt
+++ app/src/main/java/io/github/korzepadawid/chatapplication_lsm/ui/chat/ChatActivity.kt
@@ -2,15 +2,25 @@ package io.github.korzepadawid.chatapplication_lsm.ui.chat
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.text.trimmedLength
+import androidx.core.widget.addTextChangedListener
 import io.github.korzepadawid.chatapplication_lsm.R
 import io.github.korzepadawid.chatapplication_lsm.util.Constants.INTENT_RECEIVER_UID
 import io.github.korzepadawid.chatapplication_lsm.util.Constants.INTENT_RECEIVER_USERNAME
 
 class ChatActivity : AppCompatActivity() {
+
+    private lateinit var buttonSendMessage: Button
+    private lateinit var editTextMessage: EditText
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
+
 
         val receiverUid = intent.getStringExtra(INTENT_RECEIVER_UID)
         val receiverUsername = intent.getStringExtra(INTENT_RECEIVER_USERNAME)
@@ -18,5 +28,17 @@ class ChatActivity : AppCompatActivity() {
         supportActionBar?.title = receiverUsername
 
         Log.i("chat", "$receiverUsername ($receiverUid)")
+
+        buttonSendMessage = findViewById(R.id.button_send_message)
+        editTextMessage = findViewById(R.id.edit_text_message)
+
+        disableButtonWhenEmptyMessageText()
+    }
+
+    private fun disableButtonWhenEmptyMessageText() {
+        buttonSendMessage.isEnabled = false
+        editTextMessage.addTextChangedListener { text ->
+            buttonSendMessage.isEnabled = text != null && text.trimmedLength() > 0
+        }
     }
 }
